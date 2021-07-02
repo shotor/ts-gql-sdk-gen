383b805f64cec9711ad60c98b66a41a37d82b447
shotor <shotor@shotor.com>
2021-07-02 14:20:51 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ts-gql-sdk-gen
+
+Generate a complete GraphQL SDK from schema, generating all queries/mutations/subscriptions along the way.
+
+## Usage
+
+Install
+
+```sh
+yarn global add @shotor/tsgqlsdk
+```
+
+Use
+
+```sh
+tsgqlsdk /path/to/schema.graphql /path/to/destination
+```
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line
+const { version } = require('../package.json')
+import { buildSchema, printSchema, parse, GraphQLSchema } from 'graphql'
+import { codegen } from '@graphql-codegen/core'
+import { execSync } from 'child_process'
+import { Command } from 'commander'
+import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
+import { resolve } from 'path'
+import * as typescriptPlugin from '@graphql-codegen/typescript'
+import * as typescriptOperationsPlugin from '@graphql-codegen/typescript-operations'
+import * as typescriptGraphQLRequestPlugin from '@graphql-codegen/typescript-graphql-request'
+import { loadDocumentsSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+
+const program = new Command()
+
+program
+  .version(version)
+  .description('Generate full typescript sdk from graphql schema')
+
+program
+  .command('*')
+  .argument('<schema>', 'path to schema file')
+  .argument('<destination>', 'destination directory')
+  .action(async (schemaFile: string, destination: string) => {
+    if (!existsSync(schemaFile)) {
+      console.error(`Schema not found at ${schemaFile}`)
+      process.exit(1)
+    }
+
+    mkdirSync(destination, {
+      recursive: true,
+    })
+
+    // run operations generator
+    const operationsGenerator = resolve(
+      __dirname,
+      '..',
+      'node_modules',
+      '.bin',
+      'gqlg'
+    )
+
+    execSync(
+      `${operationsGenerator} --schemaFilePath ${schemaFile} --destDirPath ${destination} --depthLimit 5`
+    )
+
+    // clean up js files
+    const resolveDestination = (...paths: string[]) =>
+      resolve(destination, ...paths)
+
+    ;[
+      resolveDestination('index.js'),
+      resolveDestination('queries', 'index.js'),
+      resolveDestination('mutations', 'index.js'),
+      resolveDestination('subscriptions', 'index.js'),
+    ].forEach((f) => {
+      if (existsSync(f)) {
+        rmSync(f)
+      }
+    })
+
+    // generate sdk
+
+    const schema: GraphQLSchema = buildSchema(readFileSync(schemaFile, 'utf8'))
+
+    const outputFile = resolveDestination('gql-sdk.ts')
+
+    const output = await codegen({
+      // used by a plugin internally, although the 'typescript' plugin currently
+      // returns the string output, rather than writing to a file
+      filename: outputFile,
+      schema: parse(printSchema(schema)),
+
+      plugins: [
+        // Each plugin should be an object
+        {
+          typescript: {
+            addUnderscoreToArgsType: true,
+          }, // Here you can pass configuration to the plugin,
+        },
+        {
+          typescriptOperations: {
+            addUnderscoreToArgsType: true,
+            // operationResultSuffix: 'Foo',
+          },
+        },
+        {
+          typescriptGraphQLRequest: {},
+        },
+      ],
+      pluginMap: {
+        typescript: typescriptPlugin,
+        typescriptOperations: typescriptOperationsPlugin,
+        typescriptGraphQLRequest: typescriptGraphQLRequestPlugin,
+      },
+      documents: [
+        ...loadDocumentsSync(`${destination}/queries/*.gql`, {
+          // load from multiple files using glob
+          loaders: [new GraphQLFileLoader()],
+        }),
+        ...loadDocumentsSync(`${destination}/mutations/*.gql`, {
+          // load from multiple files using glob
+          loaders: [new GraphQLFileLoader()],
+        }),
+      ],
+      config: {
+        rawRequest: true,
+        onlyOperationTypes: true,
+        preResolveTypes: true,
+      },
+    })
+    writeFileSync(outputFile, output)
+  })
+
+program.parse(process.argv)
